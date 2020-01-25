b8d1ab96cc5fdf5c41d4e36b181698e555c24cc6
ityonemo <isaac.yonemoto@gmail.com>
2020-01-24 18:23:32 -0800

    update version number in mix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDhcp.MixProject do
   def project do
     [
       app: :ex_dhcp,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
